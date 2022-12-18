37b800150118f99d2cc4af26d3bdf4169e70de61
x <x@x>
2022-12-18 18:27:01 +0100

    moar

--- parse_imei.py
+++ parse_imei.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 html_src = open("pages/1.html").read()
 soup = BeautifulSoup(html_src, "html.parser")
+
+# Lookup a tag in HTML code:
+#
+#  <section id="section_main" class="mb-4">
+#
 print(soup.section["section_main"])
 
